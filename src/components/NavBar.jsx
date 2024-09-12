1541145379e6777a6d440f33ff1e51d2e0d02f2e
Seaaaany1804 <nejihyuga979@gmail.com>
2024-09-12 18:24:23 +0800

    Created the NavBar and HomePage

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import sLogo from '../assets/savorypack-logo.png'
+import { CiShoppingCart,CiSearch, CiUser   } from "react-icons/ci";
+
+function NavBar() {
+  const navItems = [
+    { link: "Home", path: "home" },
+    { link: "Menu", path: "menu" },
+    { link: "How it works", path: "how it works" },
+    { link: "About", path: "about" },
+    { link: "FAQs", path: "fAQs" },
+    { link: "Contact", path: "contact" },
+  ]
+
+  return (
+    <div>
+      <div className='flex justify-center space-x-[280px] items-center h-1/6 px-[120px]'>
+        <div>
+          <img src={sLogo} alt="Savory Pack Logo" className='w-[150px]'/>
+        </div>
+        
+          
+        <div className='flex justify-center'>
+          <ul className='flex space-x-14'>
+        {navItems.map((item, index) => (
+            <li key={index}>
+              <a href={`#${item.path}`} className='text-white text-xl font-poppins tracking-wider'> {item.link} </a>
+
+            </li>
+          ))}
+          </ul>
+        </div>
+
+        <div className='flex space-x-4'>
+        <CiSearch className='text-white text-4xl'/>
+        <CiShoppingCart className='text-white text-4xl'/>
+        <CiUser className='text-white text-4xl'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import foodPan from '../assets/foodpan.png'
+
+const HomePage = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <NavBar/>
+      <div className='grid grid-cols-2'>
+        <div className='flex flex-col justify-center items-center h-[80vh]'>
+          <div>
+            <h1 className='text-white font-poppins font-bold text-[80px]'> A Chef In Every <br/>
+              Tasty Meal Box </h1>
+
+            <h1 className='mt-[40px] text-white font-poppins font- text-[20px] tracking-wider'> 
+              Get pre-portioned ingredients  for Savory Pack Meal Kits <br/>
+              best recipes delivered direct to your door! </h1>
+  
+            <div className='flex space-x-8'>
+             <button type="button" className='font-poppins tracking-wide text-[18px] text-white bg-primary p-4 px-6 mt-12 rounded-xl'> 
+              Select Program </button> 
+              <button type="button" className='font-poppins tracking-wide text-white bg-transparent p-4 text-[18px] px-12 mt-12 border-[1px] border-white rounded-xl'> 
+              View Menu </button> 
+            </div>
+          </div>
+        </div>
+
+        <div className='relative'>
+          <img src={foodPan} alt="Food Pan" className='absolute w-[1000px] drop-shadow-2xl' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
